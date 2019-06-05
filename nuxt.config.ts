7e9230ba0c5bc9669b00b12f1a35542500919a08
Michael Mueller <michael.mueller@echobot.de>
2019-06-05 11:30:29 +0200

    added new icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,6 +47,8 @@ const config: NuxtConfiguration = {
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Merriweather+Sans:300,400,400i,700&display=swap" },
     ]
   },
