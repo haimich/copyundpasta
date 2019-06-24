23b921431376714a1b350f198a124d08cb71b7f9
Michael Mueller <michael.mueller@echobot.de>
2019-06-24 18:56:03 +0200

    got rid of async keyword

--- src/api/repos/articleRepo.ts
+++ src/api/repos/articleRepo.ts
@@ -3,19 +3,17 @@ import { getConnection } from "../utils/knexUtil";
 import { PagingParams } from "@/interfaces/Paging";
 import { calculateOffset } from "../utils/pagingUtil";
 
-export async function getHeroArticles(): Promise<Article[]> {
+export function getHeroArticles(): Promise<Article[]> {
   const knex = getConnection();
 
-  const articles = await knex
+  return knex
     .select("*")
     .table("articles")
     .where("isHeroArticle", 1)
     .orderBy("createdAt", "desc");
-
-  return articles;
 }
 
-export async function getArticles(params: PagingParams): Promise<Article[]> {
+export function getArticles(params: PagingParams = null): Promise<Article[]> {
   const knex = getConnection();
 
   const subselect = knex
@@ -24,13 +22,17 @@ export async function getArticles(params: PagingParams): Promise<Article[]> {
     .where("isHeroArticle", 0)
     .as("totalCount");
 
-  const articles = await knex
+  const dbObj = knex
     .select("*", subselect)
     .table("articles")
     .where("isHeroArticle", 0)
-    .limit(params.pageSize)
-    .offset(calculateOffset(params.page, params.pageSize))
     .orderBy("createdAt", "desc");
+  
+  if (params != null) {
+    dbObj
+      .limit(params.pageSize)
+      .offset(calculateOffset(params.page, params.pageSize));
+  }
 
-  return articles;
+  return dbObj;
 }
