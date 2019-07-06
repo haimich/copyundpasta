82f14ab07114a46abbbc50dfaf5d3fe3c1f4dfa7
Michael Müller <haimich@gmx.net>
2019-07-06 21:46:28 +0200

    renamed

--- knex_seeds/0_articles.ts
+++ knex_seeds/0_articles.ts
@@ -1,6 +1,6 @@
 import articles from "../src/content/articles/all";
 import recipes from "../src/content/recipes/all";
-import SearchService from "./services/SearchService";
+import SearchService from "./services/blaSearchService";
 import CategoryUtil from "../src/utils/CategoryUtil";
 
 exports.seed = async function(knex, Promise) {
