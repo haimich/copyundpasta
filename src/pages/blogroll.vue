e486598f20062aeb3caa56f8ca59202583d9e0c9
Michael Müller <haimich@gmx.net>
2019-07-24 23:19:00 +0200

    added blogs

---
+++ src/pages/blogroll.vue
@@ -0,0 +1,93 @@
+<template>
+
+  <div>
+
+    <MainHeadingComponent
+      text="Blogroll"
+      subtitle="Meine liebsten Foodblogs :)"
+    />
+
+    <el-row style="margin-top: 40px;">
+      <el-col :span="12" :offset="6">
+        <ul class="link-list">
+          <li v-for="(blog, index) in blogroll" :key="index">
+            <a :href="blog.url" rel="noopener" target="_blank">
+              {{ blog.name }}
+            </a>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script lang="ts">
+
+  import { Vue, Component, Prop } from "vue-property-decorator";
+
+  @Component
+  export default class extends Vue {
+
+    private blogroll = [
+      {
+        name: "Schöner Tag Noch",
+        url: "https://schoenertagnoch.de/",
+      },
+      {
+        name: "Schlaraffenwelt",
+        url: "https://schlaraffenwelt.de/",
+      },
+      {
+        name: "Eat this",
+        url: "https://www.eat-this.org/",
+      },
+      {
+        name: "Sallys Welt",
+        url: "https://sallys-blog.de/",
+      },
+      {
+        name: "Plötzblog",
+        url: "https://www.ploetzblog.de/",
+      },
+      {
+        name: "Die Frau am Grill",
+        url: "https://die-frau-am-grill.de/",
+      },
+      {
+        name: "Sizzle Brothers",
+        url: "https://sizzlebrothers.de/",
+      },
+      {
+        name: "Petit Appetit",
+        url: "https://www.petitappetit.de",
+      },
+    ];
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+  @import "~/scss/base.scss";
+
+  .link-list {
+    list-style: none;
+    padding: 0;
+
+    li {
+      border-left: 10px solid $color-primary;
+      padding: 20px 42px;
+      color: #121212;
+      background-color: #f5f5f5;
+      margin-bottom: 20px;
+      transition: all .2s;
+
+      &:hover {
+        font-size: 19px;
+      }
+    }
+  }
+
+</style>
