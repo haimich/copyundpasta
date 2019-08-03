fbb8fa07e68217bf63df99223e4a3fd63aa74768
Michael Müller <haimich@gmx.net>
2019-08-03 13:33:41 +0200

    improved prod setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 // config file for https://pm2.io/doc/en/runtime/overview
 module.exports = {
     apps : [{
-      name: 'cup',
-      script: './node_modules/.bin/nuxt',
-      args: 'start',
-      instances: 1,
+      name: "cup",
+      script: "./node_modules/.bin/nuxt",
+      args: "start -i 5",
+      exec_mode: "fork",
       autorestart: true,
+      exp_backoff_restart_delay: 100,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: "development"
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: "production"
       }
     }],
   };
