3b1ede1f4f3167d7dfe035f196168c4468154186
Michael Müller <haimich@gmx.net>
2019-08-06 22:20:25 +0200

    extracted newsletter component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,26 +1,39 @@
 <template>
 
   <footer>
-    <el-row style="margin-bottom: 15px; text-align: center;">
-      <el-col style="padding-left: 10px;">
-        <img src="/images/appbase.svg" alt="Appbase" style="margin-bottom: 10px; width: 163px;">
+
+    <el-row class="footer-newsletter">
+      <el-col>
+        asdas
       </el-col>
     </el-row>
 
-    <el-row>
-      <el-col :span="6" style="padding-left: 10px; display: flex; align-items: center;">
+    <el-row style="margin-top: 30px; margin-bottom: 30px; text-align: center;">
+      <el-col>
+        <a href="https://appbase.io/" target="_blank">
+          <img src="/images/appbase.svg" alt="Appbase" style="width: 163px;">
+        </a>
+      </el-col>
+    </el-row>
+
+    <el-row class="link-row">
+      <el-col :span="6" style="display: flex; align-items: center;">
         <span style="margin-right: 5px;">
           © {{ getCopyrightYears() }}
         </span>
 
         <nuxt-link to="/" title="HOME">Copy & Pasta</nuxt-link>
 
-        <img src="/favicon-32x32.png" style="width: 24px;"> 
+        <nuxt-link to="/" title="HOME">
+          <img src="/favicon-32x32.png" style="width: 24px;"> 
+        </nuxt-link>
       </el-col>
 
       <el-col :span="12" style="text-align: center;">
         <nuxt-link to="/impressum">Impressum</nuxt-link> ·
+
         <nuxt-link to="/datenschutz">Datenschutz</nuxt-link> ·
+        
         <a href="/sitemap.xml" target="_blank">Sitemap</a>
       </el-col>
 
@@ -55,16 +68,27 @@
 
 <style lang="scss" scoped>
 
+  @import "~/scss/variables.scss";
+
   footer {
-    padding: 34px 0;
+    padding: 0 0 34px 0;
     background-color: #363838;
     color: #ececec;
     font-size: 18px;
     font-weight: 300;
 
+    .link-row {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+
     a {
       color: #ececec;
     }
+  }
+
+  .footer-newsletter {
+    background-color: $color-primary;
   }
 
 </style>
--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -50,51 +50,7 @@
         Du möchtest über neue Rezepte informiert werden? Dann abonniere einfach meinen Newsletter:
       </p>
 
-      <div class="newsletter-subscribe">
-        <!-- Begin Mailchimp Signup Form -->
-        <div id="mc_embed_signup">
-          <form
-            action="https://gmx.us20.list-manage.com/subscribe/post?u=1c249484bbe9d6816f42135ee&amp;id=8d55aeb1b7"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            target="_blank"
-            novalidate
-          >
-            <div id="mc_embed_signup_scroll" style="display: flex;">
-              <el-input
-                type="email"
-                v-model="userEmail"
-                name="EMAIL"
-                id="mce-EMAIL"
-                autocomplete="on"
-                placeholder="E-Mail-Adresse"
-                @keyup.enter.native="submitEmail"
-                required
-              ></el-input>
-
-              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-              <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                <input type="text" name="b_1c249484bbe9d6816f42135ee_8d55aeb1b7" tabindex="-1" value="">
-              </div>
-
-              <div class="clear">
-                <el-button
-                  title="Abonnieren"
-                  type="info"
-                  plain
-                  icon="el-icon-message"
-                  value="Subscribe"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                  :disabled="userEmail == null || userEmail === ''"
-                  @click="submitEmail"
-                ></el-button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+      <NewsletterComponent />
     </section>
   </aside>
 
@@ -103,21 +59,14 @@
 <script lang="ts">
 
   import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+  import NewsletterComponent from "@/components/NewsletterComponent.vue";
 
-  @Component
-  export default class SidebarComponent extends Vue {
-
-    private userEmail = "";
-
-    submitEmail() {
-      const form: any = document.getElementById("mc-embedded-subscribe-form");
-
-      if (form != null) {
-        form.submit();
-      } else {
-        this.$notify.warning("Die Newsletteranmeldung ist derzeit leider nicht möglich");
-      }
+  @Component({
+    components: {
+      NewsletterComponent,
     }
+  })
+  export default class SidebarComponent extends Vue {
 
   }
 
@@ -142,22 +91,6 @@
 
       strong {
         font-weight: 900;
-      }
-    }
-
-    .newsletter-subscribe {
-      margin-top: 20px;
-
-      .el-input {
-        margin-right: 10px;
-      }
-
-      .el-button {
-        padding: 8px 12px;
-        color: white;
-        background-color: $color-primary;
-        border: none;
-        font-size: 23px;
       }
     }
   }
