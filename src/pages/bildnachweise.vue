740f19c8fb516a5888654152a947a2e44839c88c
Michael Müller <haimich@gmx.net>
2019-08-06 23:25:17 +0200

    added

---
+++ src/pages/bildnachweise.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+
+    <MainHeadingComponent
+      text="Bildnachweise"
+    />
+
+    <el-row style="margin-top: 20px;">
+      <el-col :span="12" :offset="6">
+        
+        <ul>
+          <li>
+            Footer-Hintergrund:
+            <a href="https://de.freepik.com/pikisuperstar" target="_blank" rel="noopener">https://de.freepik.com/pikisuperstar</a>
+          </li>
+        </ul>
+
+      </el-col>
+    </el-row>
+
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
