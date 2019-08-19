e01cbff71c6340782174eb3fcd2a110ecd68ead5
Michael Müller <haimich@gmx.net>
2019-08-19 21:54:25 +0200

    wrote descriptions

--- src/pages/blogroll.vue
+++ src/pages/blogroll.vue
@@ -5,14 +5,13 @@
       <el-col :span="14" :offset="5">
         <MainHeadingComponent
           text="Blogroll"
-          subtitle="Hier findet ihr meine liebsten Foodblogs :)"
+          subtitle="Hier findet ihr meine liebsten Foodblogs :-)"
         />
         
         <ul class="link-list">
           <li
             v-for="(blog, index) in blogroll"
             :key="index"
-            @click="openLink(blog.url)"
             title="Link öffnen"
           >
             <el-row :gutter="20">
@@ -21,6 +20,7 @@
                   :src="blog.image"
                   :title="blog.name"
                   class="blog-image"
+                  @click="openLink(blog.url)"
                 ></el-image>
               </el-col>
               
@@ -34,9 +34,7 @@
                   {{ blog.name }}
                 </a>
 
-                <div style="margin-top: 8px; font-size: 15px;">
-                  {{ blog.description }}
-                </div>
+                <div class="blog-description" v-html="blog.description"></div>
               </el-col>
             </el-row>
           </li>
@@ -57,25 +55,34 @@
     private blogroll = [
       {
         name: "Schöner Tag noch!",
-        description: "Der Food-Blog aus Karlsruhe",
+        description: `Bevor ich in die Region Karlsruhe gezogen bin, hätte ich es wohl selbst nicht für möglich gehalten, dass man hierzulande in der Bäckerei wirklich mit den Worten &bdquo;Schöner Tag noch!&ldquo; verabschiedet wird. 
+<br/>Korrekte Grammatik hin oder her - Julianes Blog empfehle ich sehr gerne weiter, denn ich habe schon viele ihrer Rezepte erfolgreich nachgekocht!`,
         image: "/images/blogroll/schoenertagnoch.jpg",
         url: "https://schoenertagnoch.de/",
       },
       {
         name: "Schlaraffenwelt",
-        description: "Expedition Genuss - Ein Magazin für weltoffene Genießer",
+        description: `Dieser Blog stellt sowohl inhaltlich als auch designtechnisch ein großes Vorbild für mich dar. Die Gerichte sind äußerst ansprechend fotografiert und die Rezepte der &bdquo;Schlaraffenwelt&ldquo; beleuchten ein Thema oft sehr detailreich, wie zum Beispiel der Beitrag <a href="https://schlaraffenwelt.de/perfektes-wiener-schnitzel-rezept/">Das Perfekte Schnitzel</a> zeigt.`,
         image: "/images/blogroll/schlaraffenwelt.jpg",
         url: "https://schlaraffenwelt.de/",
       },
       {
+        name: "Küchenchaotin",
+        description: `Ich bin der vegetarischen Küche durchaus offen gegenüber und das verdanke ich nicht zuletzt der &bdquo;Küchenchaotin&ldquo;.
+<br/>Egal welches Rezept ich von Mia ausprobiere, es erntet stets großes Lob und wird in meine Rezeptsammlung aufgenommen.`,
+        image: "/images/blogroll/kuechenchaotin.jpg",
+        url: "https://kuechenchaotin.de/",
+      },
+      {
         name: "Eat this",
-        description: "Foodblog | Gesunde, vegane Rezepte • Food Fotografie • Stories",
+        description: `Wer auf vegane Küche steht, wird sicher im Blog &bdquo;Eat this&ldquo; fündig. Beim Anblick der perfekt abgelichteten Rezeptfotos läuft einem das Wasser im Mund zusammen.
+<br/>Hier kommen auch Nichtveganer im wahrsten Sinne des Wortes auf ihren Geschmack!`,
         image: "/images/blogroll/eatthis.jpg",
         url: "https://www.eat-this.org/",
       },
       {
         name: "Sallys Welt",
-        description: "Über Sally muss man glaube ich nicht mehr viel sagen, da ihre Produkte mittlerweile sogar in vielen Supermärkten zu finden ist. Sie ist die mit Abstand erfolgreichste Foodbloggerin Deutcshlands.",
+        description: "Über Sally muss man glaube ich nicht mehr viel sagen, da ihre Produkte es mittlerweile sogar in viele Supermarktregale geschafft haben. Sie ist die mit Abstand (und zurecht) Deutschlands erfolgreichste Foodbloggerin.",
         image: "/images/blogroll/sallysblog.jpg",
         url: "https://sallys-blog.de/",
       },
@@ -142,7 +149,11 @@
       background-color: #f5f5f5;
       margin-bottom: 20px;
       transition: all .2s;
-      cursor: pointer;
+
+      .blog-image {
+        min-height: 155px;
+        cursor: pointer;
+      }
 
       &:hover {
         font-size: 19px;
@@ -151,8 +162,10 @@
     }
   }
 
-  .blog-image {
-    min-height: 155px;
+  .blog-description {
+    margin-top: 8px;
+    font-size: 16px;
+    color: #252525;
   }
 
 </style>
