3a4b3e6538e561e50568042922f850d67fc3d5c8
Michael Müller <haimich@gmx.net>
2019-11-12 23:10:41 +0100

    added recipe

--- src/content/recipes/all.ts
+++ src/content/recipes/all.ts
@@ -1,7 +1,9 @@
 import r1 from "./zimtschnecken-aka-cinnamon-rolls";
 import r2 from "./one-pot-pasta-mit-leckerer-tomatensauce";
+import r3 from "./moehren-quiche-mit-bergkaese";
 
 export default [
   r1,
   r2,
+  r3,
 ];
